6f0ab6a0d2f7e11facec183be3420d723548399b
Trent Howard <trent.howard.42@gmail.com>
2019-11-09 11:39:10 +1100

    Add manifest for deployment Deploy exit if manifest item missing
    Co-authored-by: yazz <yeltahir@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,64 @@
-# *Assumes starting in root*
+#!/bin/bash
+
+MANIFEST="deploy_manifest"
+DEPLOY_PACKAGE="ZootAPI.zip"
+
+if [ -f ./${MANIFEST} ]
+then 
+    source ${MANIFEST}
+else
+    echo "Unable to find ./${MANIFEST}"
+    exit 1
+fi
 
 # Clean up from past releases
 if [ -d "package" ]; then rm -Rf package; fi
-if [ -f "ZootAPI.zip" ]; then rm -Rf ZootAPI.zip; fi
+if [ -f "${DEPLOY_PACKAGE}" ]; then rm -Rf ${DEPLOY_PACKAGE}; fi
 
-# Create directory to house the libraries
-mkdir package
-cd package
-
-# Install requirements & create zip
+# Create directory to house the libraries and install requirements
+mkdir ./package
+pushd ./package &>/dev/null
 pip install -r ../requirements.txt --target .
-zip -r9 ../ZootAPI.zip .
+popd &>/dev/null
 
-# Add lambda function & custom dependencies 
-cd ../
-zip -g ZootAPI.zip zootapi.py gigmanagement.py conf.yaml
+# Create working directory
+mkdir ./tmp
+
+# Check each item in the manifest exists, if not error out
+NUM_DIR=${#DIR[@]}
+for ((i=0; i<$NUM_DIR; i++))
+do
+    if [ ! -d "${DIR[i]}" ]
+    then
+        echo "Unable to find required directory ${DIR[i]}"
+        rm -r ./tmp && exit 1
+    else
+        cp -r ./${DIR[i]} ./tmp
+    fi
+done
+
+NUM_FILE=${#FILE[@]}
+for ((i=0; i<$NUM_FILE; i++))
+do
+    if [ ! -f "${FILE[i]}" ]
+    then
+        echo "Unable to find required file ${FILE[i]}"
+        rm -r ./tmp && exit 1
+    else
+        cp ./${FILE[i]} ./tmp
+    fi
+done
+
+# Package it up
+pushd ./tmp &>/dev/null
+zip -r9 ../${DEPLOY_PACKAGE} .
+popd &>/dev/null
+
+if [ ! -f "./${DEPLOY_PACKAGE}" ]
+then
+    echo "Unable to create package for deployment."
+    rm -r ./tmp && exit 1
+fi
 
 # Push to AWS
 aws lambda update-function-code --publish --function-name Get-Gigs --zip-file fileb://ZootAPI.zip
