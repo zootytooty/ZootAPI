645a9551541a08cfa961ddb327df848034ec7808
Trent Howard <trent.howard.42@gmail.com>
2021-03-21 12:44:03 +1100

    Handy to be able to return the model as a dict

--- src/api/gigs/gig_model.py
+++ src/api/gigs/gig_model.py
@@ -4,23 +4,30 @@ from datetime import datetime
 from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
 from pynamodb.models import Model
 
-class GigModel(Model):
-  class Meta:
-    table_name = 'gigs'
-    if 'IS_OFFLINE' in os.environ:
-      host = 'http://localhost:8000'
-    else:
-      region = 'ap-southeast-2'
-      host = 'https://dynamodb.ap-southeast-2.amazonaws.com'
 
-  gig_id = UnicodeAttribute(hash_key=True, null=False)
-  title = UnicodeAttribute(null=False)
-  music_starts = UnicodeAttribute(null=True) 
-  doors_open = UnicodeAttribute(null=False)
-  performance_date = UTCDateTimeAttribute(null=False)
-  price = UnicodeAttribute(null=True) 
-  description = UnicodeAttribute(null=True) 
-  url = UnicodeAttribute(null=False) 
-  image_url = UnicodeAttribute(null=True) 
-  createdAt = UTCDateTimeAttribute(null=False, default=datetime.now())
-  updatedAt = UTCDateTimeAttribute(null=True)
+class GigModel(Model):
+    class Meta:
+        table_name = "gigs"
+        if "IS_OFFLINE" in os.environ:
+            host = "http://localhost:8000"
+        else:
+            region = "ap-southeast-2"
+            host = "https://dynamodb.ap-southeast-2.amazonaws.com"
+
+    def as_dict(self):
+        """
+        Takes the current model and reviews the attributes to then translate to a dict
+        """
+        return {key: getattr(self, key) for key in self._get_attributes().keys()}
+
+    gig_id = UnicodeAttribute(hash_key=True, null=False)
+    title = UnicodeAttribute(null=False)
+    music_starts = UnicodeAttribute(null=True)
+    doors_open = UnicodeAttribute(null=False)
+    performance_date = UTCDateTimeAttribute(null=False)
+    price = UnicodeAttribute(null=True)
+    description = UnicodeAttribute(null=True)
+    url = UnicodeAttribute(null=False)
+    image_url = UnicodeAttribute(null=True)
+    createdAt = UTCDateTimeAttribute(null=False, default=datetime.now())
+    updatedAt = UTCDateTimeAttribute(null=True)
