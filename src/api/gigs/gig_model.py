eaf338957f19a07e0b4dfb5cd8e707cb4d9743e6
Trent Howard <trent.howard.42@gmail.com>
2021-03-21 12:11:03 +1100

    Use the right variable name, updatedAt can be null

--- src/api/gigs/gig_model.py
+++ src/api/gigs/gig_model.py
@@ -7,7 +7,7 @@ from pynamodb.models import Model
 class GigModel(Model):
   class Meta:
     table_name = 'gigs'
-    if 'ENV' in os.environ:
+    if 'IS_OFFLINE' in os.environ:
       host = 'http://localhost:8000'
     else:
       region = 'ap-southeast-2'
@@ -23,4 +23,4 @@ class GigModel(Model):
   url = UnicodeAttribute(null=False) 
   image_url = UnicodeAttribute(null=True) 
   createdAt = UTCDateTimeAttribute(null=False, default=datetime.now())
-  updatedAt = UTCDateTimeAttribute(null=False)
+  updatedAt = UTCDateTimeAttribute(null=True)
