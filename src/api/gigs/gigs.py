16a7684d581071a0709bda719110b27947b6e056
Trent Howard <trent.howard.42@gmail.com>
2021-03-27 21:29:25 +1100

    Add handler and endpoint to delete gigs

--- src/api/gigs/gigs.py
+++ src/api/gigs/gigs.py
@@ -96,11 +96,33 @@ def get(event, context):
 
     try:
         gig = GigModel.get(gig_id)
-
         response["statusCode"] = 200
         response["body"] = json.dumps(gig.as_dict(), default=str)
-        return response
+    except:
+        response["statusCode"] = 404
+        response["body"] = json.dumps({"error": f"Gig with id '{gig_id}' not found"})
+    
+    return response
+
+def delete(event, context):
+    response = {}
+    gig_id = event["pathParameters"]["id"]
+
+    try:
+        gig = GigModel.get(gig_id)
     except:
         response["statusCode"] = 404
         response["body"] = json.dumps({"error": f"Gig with id '{gig_id}' not found"})
         return response
+    try:
+        deleted_gig = gig.as_dict()
+        gig.delete()
+        response["statusCode"] = 200
+        response["body"] = json.dumps({"deleted": deleted_gig}, default=str)
+    except:
+        response["statusCode"] = 400
+        response["body"] = json.dumps({"error": f"Unable to delete gig '{gig_id}'"})
+        return response
+    
+    return response
+        
